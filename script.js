45c36fae2d86134b71118f16e2f05c99757d5658
jadeemttz <jmartinezh2211@alumno.ipn.mx>
2024-10-15 18:32:58 -0600

    Clase 15-10-2024

---
+++ script.js
@@ -0,0 +1,110 @@
+//definicion de una funcion;
+/*
+
+function hello(){
+    console.log('Hola clase');
+    console.log('5IV7');
+}
+
+//llamada a la funcion
+hello();
+hello();
+hello();
+hello();
+hello();
+*/
+/*
+function hello(){
+    return 'Hola clase'
+}
+
+const result = hello()
+
+console.log(result);
+*/
+
+//funcion que retorna otra funcion
+/*
+function hello(){
+    return function(){
+        return 'Hola soy la funcion 2';
+    }
+}
+
+console.log(hello()());
+*/
+/*
+function add(x){
+    console.log(x);
+}
+    add(5)
+    */
+/*
+    function add(x,y=0){
+        if(y===undefined){
+            y=0
+        }
+        console.log(x+y);
+    }
+
+    add(5,undefined)
+    */
+
+    /*
+    //parametro de tipo cadena 
+    function add(name){
+        console.log('Hola' + name);
+    }
+    
+    add("Clase 35");
+    */
+
+    //OBJETOS
+    const user = {
+        nombre:'Jade',
+        apellidoP:'Martinez',
+        apellidoM:'Hernandez',
+        edad:17,
+        direccion:{
+            calle:'Lago',
+            no:93,
+            colonia:'Narvarte',
+            delegacion:'Venustiano Carranza'},
+            amigos: ['Cath','Jali'],
+            activo:true
+            }
+console.log(user);
+            //consola nombre apellidoP apellidoM
+            //alert activo;
+            //console edad
+            //alert calle no colonia delegacion
+            //console amigos
+
+const nombre = {
+    nombre:'Jade',
+        apellidoP:'Martinez',
+        apellidoM:'Hernandez',
+        
+}
+console.log(nombre);
+alert('Status: Activo')
+const edad = {
+    edad:17
+}
+console.log(edad);
+alert('Calle: Lago, No:93, Colonia: Narvarte, Delegacion: Venustiano Carranza')
+const amigos={
+    amigo1:'Cath',
+    amigo2:'Jali'
+}
+console.log(amigos);
+    /*
+    []
+    */
+/*
+    ------------
+    */
+
+    /*
+  
+*/
